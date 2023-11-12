543cdcc8d7a9f0910bc2256480d76d41a70678da
Henriquedearaujo <ricke_cris@hotmail.com>
2023-11-12 14:52:50 -0300

    primeiro commit

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # Agenda-contato
+
+
+## você irá criar uma aplicação de agenda de contatos.
+
+1) Construa uma tabela com duas colunas: nome e telefone.
+
+2) Construa um formulário no qual o usuário irá inserir o nome e telefone do contato e, ao clicar em cadastrar, uma linha será adicionada na tabela de contatos.
+
+3) Não permita a inserção de um número de telefone repetido
+
+## Objetivo
+
+> entender o benefício de usar o JQuery ao desenvolver aplicações web;
+
+> aplica seletores apropriados no jQuery;
+
+> usa métodos e funções do JQuery para realizar manipulações nos elementos do DOM;
+
+> identifica diferentes tipos de eventos;
+
+> aplica eventos a elementos;
+
+> cria comportamentos interativos específicos para cada tipo de evento;
+
+> usa funções para aplicar efeitos de expansão e recolhimento a elementos;
+
+> pratica a seleção de elementos do JQuery;
+
+> usa animações de slideUp e slideDown;
+
+> compreender o conceito de manipulação do DOM usando a biblioteca jQuery;
+
+> cria elementos como imagens, links e divs usando métodos e funções do jQuery;
+
+> aplica animações e efeitos visuais aos elementos HTML criados dinamicamente;
+
+> realiza os passos necessários para fazer o deploy de um projeto no GitHub; 
+
+> ajusta configurações e detalhes no GitHub;
+
+> compreender a importância da compressão de imagens.
---
+++ main.js
@@ -0,0 +1,60 @@
+const form = document.getElementById('form-atividade');
+
+const nomes = [];
+const telefones = [];
+
+let linhas = "";
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionarLinha()
+    atulizarTabela()
+    
+    
+});
+
+function adicionarLinha() {
+    const inputNome = document.getElementById('nome-completo');
+    const inputTelefone = document.getElementById('numero-telefone');
+
+    if (telefones.includes(parseFloat(inputTelefone.value))) {
+        alert(`Telefone: ${inputNome.value} já foi inserida`);
+    } else {
+
+        nomes.push(inputNome.value);
+        telefones.push(parseFloat(inputTelefone.value));
+        
+        let linha = `<tr>`;
+        linha += `<td>${inputNome.value}</td>`;
+        linha += `<td>${inputTelefone.value}</td>`;
+        linha += `</tr>`;
+    
+        linhas += linha;
+    }
+
+
+    inputNome.value = '';
+    inputTelefone.value = '';
+}
+
+function atulizarTabela() {
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = linhas;
+}
+//identifica se é número fixo ou celular e já permite a quantidade de caracteres necessárias
+const maskPhoneNumber = (value) => {
+    if (!value) return ''
+  
+    return value
+      .replace(/[\D]/g, '')
+      .replace(/(\d{2})(\d)/, '($1) $2')
+      .replace(value[5] != 9 ? /(\d{4})(\d)/ : /(\d{5})(\d)/, '$1-$2')
+      .replace(/(-\d{4})(\d+?)/, '$1')
+  }
+  
+  const applyPhoneNumberMask = (input) => {
+    input.value = maskPhoneNumber(input.value)
+  }
+
+
